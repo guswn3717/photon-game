3ceeccc39e65d5aa8f62e480c0fee6b4393731e9
guswn3717 <rudals9973@gmail.com>
2025-06-14 12:00:00 +0900

    20250614

--- Photon Network Game/Assets/Scripts/LobbyManager.cs
+++ Photon Network Game/Assets/Scripts/LobbyManager.cs
@@ -7,5 +7,47 @@ using Photon.Realtime;
 
 public class LobbyManager : MonoBehaviourPunCallbacks
 {
+    [SerializeField] Transform parentTransform;
+
+    [SerializeField] Dictionary<string, GameObject> dictionary = new Dictionary<string, GameObject>();
    
+    public void OnCreateRoom()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+
+        roomOptions.MaxPlayers = 4;
+
+        roomOptions.IsOpen = true;
+
+        roomOptions.IsVisible = true;
+
+        PhotonNetwork.CreateRoom("Battle", roomOptions);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Game");
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        GameObject prefab = null;
+
+        foreach(RoomInfo room in roomList)
+        {
+            // 룸이 삭제된 경우
+            if(room.RemovedFromList == true)
+            {
+                dictionary.TryGetValue(room.Name, out prefab);
+
+                Destroy(prefab);
+
+                dictionary.Remove(room.Name);
+            }
+            else // 룸의 정보가 변경되는 경우
+            {
+
+            }
+        }
+    }
 }
