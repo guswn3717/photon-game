f13a058fd27c12d7e5ad271d077cb165cc13f098
guswn3717 <rudals9973@gmail.com>
2025-06-18 12:00:00 +0900

    20250618

--- Photon Network Game/Assets/Scripts/Character.cs
+++ Photon Network Game/Assets/Scripts/Character.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 public class Character : MonoBehaviourPun
 {
     [SerializeField] float speed;
+    [SerializeField] float mouseX;
+    [SerializeField] float rotationSpeed;
+
     [SerializeField] Vector3 direction;
 
     [SerializeField] Camera virtualCamera;
@@ -26,6 +29,8 @@ public class Character : MonoBehaviourPun
             Control();
 
             Move();
+
+            Rotate();
         }
     }
 
@@ -35,12 +40,20 @@ public class Character : MonoBehaviourPun
         direction.z = Input.GetAxisRaw("Vertical");
 
         direction.Normalize();
+
+        // mouseX에 마우스로 입력한 값을 저장합니다.
+        mouseX += Input.GetAxisRaw("Mouse X") * rotationSpeed * Time.deltaTime;
     }
 
     public void Move()
     {
         // 방향 * 속도 * 시간
-        characterController.Move(direction * speed * Time.deltaTime);
+        characterController.Move(characterController.transform.TransformDirection(direction) * speed * Time.deltaTime);
+    }
+
+    public void Rotate()
+    {
+        transform.eulerAngles = new Vector3(0, mouseX, 0);
     }
 
     public void DisableCamera()
@@ -53,6 +66,8 @@ public class Character : MonoBehaviourPun
         else
         {
             virtualCamera.gameObject.SetActive(false);
+
+            virtualCamera.GetComponent<AudioListener>().enabled = false;
         }
     }
 }
